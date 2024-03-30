64cbe73b236bdfc25a1d3a2bb848737571f8441f
Emmett Rogers <eprogers15@icloud.com>
2024-03-30 17:12:15 -0400

    Add sloppydrinks.com to ALLOWED_HOSTS

--- sloppy_drinks/sloppy_drinks/settings.py
+++ sloppy_drinks/sloppy_drinks/settings.py
@@ -33,7 +33,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sloppydrinks.com']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
