1002ceca37037a283ae67f1af0ee958a02dbcc68
Emmett Rogers <eprogers15@icloud.com>
2024-04-06 17:27:31 -0400

    Change how navbar height is calculated to set body padding

--- sloppy_drinks/drinks/static/drinks/javascript/index.js
+++ sloppy_drinks/drinks/static/drinks/javascript/index.js
@@ -1,11 +1,11 @@
 // Set initial starting point of body content based on navbar height
-let padding = $("nav").height() + 16 + "px";
+let padding = $("nav").outerHeight(true) + "px";
 $("body").css("paddingTop", padding);
 
 // Reset starting point of body content based on navbar height after button click
 $(".navbar-toggler").click(function () {
   setTimeout(function () {
-    let padding = $("nav").height() + 16 + "px";
+    let padding = $("nav").outerHeight(true) + "px";
     $("body").css("paddingTop", padding);
   }, 500);
 });
@@ -13,7 +13,7 @@ $(".navbar-toggler").click(function () {
 // Reset starting point of body content based on navbar height after window resizing
 $(window).resize(function () {
   setTimeout(function () {
-    let padding = $("nav").height() + 16 + "px";
+    let padding = $("nav").outerHeight(true) + "px";
     $("body").css("paddingTop", padding);
   }, 500);
 });
