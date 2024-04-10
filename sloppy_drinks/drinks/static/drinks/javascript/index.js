c01087e2f20cab1ba8ab29094126894f479d7d8f
Emmett Rogers <eprogers15@icloud.com>
2024-04-09 20:33:07 -0400

    Set initial body padding-top in styles.css instead of index.js

--- sloppy_drinks/drinks/static/drinks/javascript/index.js
+++ sloppy_drinks/drinks/static/drinks/javascript/index.js
@@ -1,3 +1,19 @@
+// Reset starting point of body content based on navbar height after button click
+$(".navbar-toggler").click(function () {
+  setTimeout(function () {
+    let padding = $("nav").outerHeight(true) + "px";
+    $("body").css("paddingTop", padding);
+  }, 500);
+});
+
+// Reset starting point of body content based on navbar height after window resizing
+$(window).resize(function () {
+  setTimeout(function () {
+    let padding = $("nav").outerHeight(true) + "px";
+    $("body").css("paddingTop", padding);
+  }, 500);
+});
+
 $(".dropdown-item").click(function() {
   let new_sort_order = this.attributes.value.value;
   $("#sort-button").attr("value", new_sort_order);
