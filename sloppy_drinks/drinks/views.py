93510f92fb61e039bdcae1f27e4ef25c6d33d62e
Emmett Rogers <eprogers15@icloud.com>
2024-04-30 10:06:31 -0400

    Complete filter query and integrate it with sort and search

--- sloppy_drinks/drinks/views.py
+++ sloppy_drinks/drinks/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Q, Prefetch, Min, Count
+from django.db.models import Q, Prefetch, Min, Count, Subquery
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from drinks.models import Drink, Ingredient, Image, Episode
@@ -21,17 +21,23 @@ def drink_index(request):
 def drink_index_partial(request):
     search = request.GET.get('q')
     sort = request.GET.get('sort')
-    if request.GET.get('filter'):
-        filter = request.GET.get('filter').split(",")
+    filter = request.GET.get('filter')
+    if filter:
+        filter = filter.split(",")
     page_num = request.GET.get('page', 1)
 
-    drinks = Drink.objects.all()
+    ingredients_filter_subquery = Drink.objects.filter(ingredients__name__in=filter).annotate(num_filter_ingredients=Count('name')).filter(num_filter_ingredients=len(filter)).values('name')
 
-    if search:
-        drinks = drinks.filter(Q(name__icontains=search) | Q(ingredients__name__icontains=search))
+    search_filter_subquery = Drink.objects.filter(Q(name__icontains=search) | Q(ingredients__name__icontains=search)).values('name')
 
-    if len(filter) > 0:
-        drinks = drinks.filter(ingredients__name__in=filter).annotate(num_filter_ingredients=Count('name')).filter(num_filter_ingredients=len(filter))
+    if search and filter:
+        drinks = Drink.objects.filter(name__in=ingredients_filter_subquery).filter(name__in=search_filter_subquery)
+    elif search and not filter:
+        drinks = Drink.objects.filter(name__in=Subquery(search_filter_subquery))
+    elif filter and not search:
+        drinks = Drink.objects.filter(name__in=Subquery(ingredients_filter_subquery))
+    else:
+        drinks = Drink.objects.all()
     
     drinks = drinks.prefetch_related(Prefetch('episode_set', queryset=Episode.objects.all(), to_attr="episode_number"), Prefetch('image_set', Image.objects.filter(recipe=True), to_attr="recipe_image")).annotate(episode_number=Min('episode__number')).order_by(sort)
         
