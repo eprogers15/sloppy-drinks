10ead875084414bf842f543e597c12c1d2030841
Emmett Rogers <eprogers15@icloud.com>
2025-10-30 14:26:11 -0400

    Remove duplicate of django.middleware.security.SecurityMiddleware in settings.py

--- sloppy_drinks/sloppy_drinks/settings.py
+++ sloppy_drinks/sloppy_drinks/settings.py
@@ -62,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django_htmx.middleware.HtmxMiddleware',
-    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
